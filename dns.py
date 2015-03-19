756e9119b2e39ab4529073c70aa09c07c348ad2d
zhao-ji <iamfrommop@gmail.com>
2015-03-19 19:37:43 +0800

    feature: DNS shunt

--- dns.py
+++ dns.py
@@ -17,15 +17,25 @@ class ProxyHandler(DatagramRequestHandler):
     '''DNS Proxy Server'''
     def handle(self):
         data, client = self.request
-        logbook.info(
-            "query name: {}".format(DNSRecord.parse(data).q.qname))
 
-        remote = socket.socket(
-            socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-        identifier = self.client_address[1]
-        send_packet = icmp.pack(identifier, 53, data)
-        remote.sendto(send_packet, (args.remote, 1))
-        dns_body = icmp.unpack(remote.recv(8192))
+        domain = str(DNSRecord.parse(data).q.qname)
+        logbook.info("query name: {}".format(domain))
+
+        basic_domain = ".".join(domain.rstrip(".").split(".")[-2:])
+        logbook.info(basic_domain)
+        if basic_domain in CHINA_DOMAIN_LIST:
+            logbook.info("Go dirty DNS")
+            sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+            sock.sendto(data, (args.dirty, 53))
+            dns_body, _ = sock.recvfrom(8192)
+        else:
+            logbook.info("Go ICMP DNS")
+            remote = socket.socket(
+                socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+            identifier = self.client_address[1]
+            send_packet = icmp.pack(identifier, 53, data)
+            remote.sendto(send_packet, (args.remote, 1))
+            dns_body = icmp.unpack(remote.recv(8192))
 
         ip_list = "\n".join(
             [str(r.rdata) for r in DNSRecord.parse(dns_body).rr]
@@ -49,13 +59,17 @@ if __name__ == "__main__":
         "--remote", "-r", default="chashuibiao.org",
         metavar="<remote dns server>",
         help="Upstream DNS server (default:chashuibiao.org)")
+    p.add_argument(
+        "--dirty", "-d", default="114.114.114.114",
+        metavar="<china dirty dns server>",
+        help="Dirty DNS server (default:114.114.114.114)")
     args = p.parse_args()
+
+    from china_domain import china_domain_list as CHINA_DOMAIN_LIST
 
     server = ForkingUDPServer((args.address, args.port), ProxyHandler)
     logbook.info(
         "Start proxy server at {}:{}"
         .format(args.address, args.port))
-    logbook.info(
-        "Connect DNS server at {}".format(args.remote))
 
     server.serve_forever()
