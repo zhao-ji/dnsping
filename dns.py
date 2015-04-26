85bec1b20c134259b5f7635cddf380a471be14c0
yulin <byteswarning@gmail.com>
2015-04-26 11:46:23 +0800

    feature: change the china list to txt file; filter the .cn domain; replace the Forked server with Threaded server; and the socket timeout mechanism

--- dns.py
+++ dns.py
@@ -3,8 +3,8 @@
 
 import argparse
 import socket
-from SocketServer import ForkingUDPServer, DatagramRequestHandler
-import struct
+from SocketServer import ThreadingUDPServer, DatagramRequestHandler
+# import struct
 
 
 from dnslib import DNSRecord
@@ -23,11 +23,16 @@ class ProxyHandler(DatagramRequestHandler):
 
         basic_domain = ".".join(domain.rstrip(".").split(".")[-2:])
         logbook.info(basic_domain)
-        if basic_domain in CHINA_DOMAIN_LIST:
+        if basic_domain in CHINA_DOMAIN_LIST or basic_domain.endswith("cn"):
             logbook.info("Go dirty DNS")
-            sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             sock.sendto(data, (args.dirty, 53))
-            dns_body, _ = sock.recvfrom(8192)
+            try:
+                dns_body, _ = sock.recvfrom(8192)
+            except socket.timeout:
+                logbook.warning("timeout")
+                client.close()
+                return
         else:
             logbook.info("Go ICMP DNS")
             remote = socket.socket(
@@ -35,7 +40,12 @@ class ProxyHandler(DatagramRequestHandler):
             identifier = self.client_address[1]
             send_packet = icmp.pack(identifier, 53, data)
             remote.sendto(send_packet, (args.remote, 1))
-            dns_body = icmp.unpack(remote.recv(8192))
+            try:
+                dns_body = icmp.unpack(remote.recv(8192))
+            except:
+                logbook.warning("timeout")
+                client.close()
+                return
 
         try:
             ip_list = "\n".join(
@@ -52,7 +62,7 @@ class ProxyHandler(DatagramRequestHandler):
 if __name__ == "__main__":
     p = argparse.ArgumentParser(description="DNS Proxy")
     p.add_argument(
-        "--port","-p", type=int, default=53,
+        "--port", "-p", type=int, default=53,
         metavar="<port>",
         help="Local proxy port (default:53)")
     p.add_argument(
@@ -69,9 +79,12 @@ if __name__ == "__main__":
         help="Dirty DNS server (default:114.114.114.114)")
     args = p.parse_args()
 
-    from china_domain import china_domain_list as CHINA_DOMAIN_LIST
+    china_list_file = open("./china_domain.txt")
+    CHINA_DOMAIN_LIST = china_list_file.read().split('\n')
+    china_list_file.close()
 
-    server = ForkingUDPServer((args.address, args.port), ProxyHandler)
+    socket.setdefaulttimeout(1)
+    server = ThreadingUDPServer((args.address, args.port), ProxyHandler)
     logbook.info(
         "Start proxy server at {}:{}"
         .format(args.address, args.port))
